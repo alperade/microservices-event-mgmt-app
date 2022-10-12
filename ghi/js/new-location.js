cb334b30dddd23df4588fd9ec5697078d197c31e
alper ademoglu <alperademoglu@hotmail.com>
2022-10-12 16:54:42 -0400

    Added create conference form.

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -1,47 +1,45 @@
-console.log("success")
-
 window.addEventListener('DOMContentLoaded', async () => {
-    const url = 'http://localhost:8000/api/states/';
+  const url = 'http://localhost:8000/api/states/';
 
-    try {
-        const response = await fetch(url);
+  try {
+      const response = await fetch(url);
 
-        if (!response.ok) {
-          alert('Response not ok!', 'danger');
-          throw new Error('Response not ok');
-        } else {
-          const data = await response.json();
-          const state = document.getElementById("state");
-          for (let abb of data.states) {
-            var option = document.createElement("option");
-            option.value = abb.abbreviation
-            option.innerHTML = abb.name
-            state.appendChild(option);
-          }
+      if (!response.ok) {
+        alert('Response not ok!', 'danger');
+        throw new Error('Response not ok');
+      } else {
+        const data = await response.json();
+        const state = document.getElementById("state");
+        for (let abb of data.states) {
+          var option = document.createElement("option");
+          option.value = abb.abbreviation
+          option.innerHTML = abb.name
+          state.appendChild(option);
         }
-      } catch (e) {
-          alert('Error', 'warning');
-          console.error('error', e)
       }
+    } catch (e) {
+        alert('Error', 'warning');
+        console.error('error', e)
+    }
 
-    });
+  });
 
-        const formTag = document.getElementById('create-location-form');
-        formTag.addEventListener('submit', async event => {
-            event.preventDefault();
-            const formData = new FormData(formTag);
-            const json = JSON.stringify(Object.fromEntries(formData));
-            const locationUrl = 'http://localhost:8000/api/locations/';
-            const fetchConfig = {
-                method: "POST",
-                body: json,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            };
-            const response = await fetch(locationUrl, fetchConfig);
-            if (response.ok) {
-                formTag.reset();
-                const newLocation = await response.json();
-                }
-                    });
+      const formTag = document.getElementById('create-location-form');
+      formTag.addEventListener('submit', async event => {
+          event.preventDefault();
+          const formData = new FormData(formTag);
+          const json = JSON.stringify(Object.fromEntries(formData));
+          const locationUrl = 'http://localhost:8000/api/locations/';
+          const fetchConfig = {
+              method: "POST",
+              body: json,
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+          };
+          const response = await fetch(locationUrl, fetchConfig);
+          if (response.ok) {
+              formTag.reset();
+              const newLocation = await response.json();
+              }
+                  });
