d5f7b25625e3c49c231c7a5bf913fcc5d7ec1232
alper ademoglu <alperademoglu@hotmail.com>
2022-10-12 15:27:20 -0400

    Added form to add location.

--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -228,3 +228,15 @@ def api_show_location(request, pk):
             encoder=LocationDetailEncoder,
             safe=False,
         )
+
+@require_http_methods(["GET"])
+def api_list_states(request):
+    states = State.objects.all().order_by('name')
+    state_list = []
+    for state in states:
+        dict = {}
+        dict["name"] = state.name
+        dict["abbreviation"] = state.abbreviation
+        state_list.append(dict)
+    return JsonResponse(
+        {"states": state_list})
